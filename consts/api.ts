750a82c17f9782b43f0576bc1c39c2308f42251f
p.niezgoda <p.niezgoda@hotmail.com>
2025-12-05 22:51:46 +0100

    use railway api

--- consts/api.ts
+++ consts/api.ts
@@ -1,4 +1,5 @@
-const HOST = 'http://127.0.0.1:5000';
+const HOST = 'https://common-shopping-list-api-production.up.railway.app';
+const HOST_LOCAL = 'http://127.0.0.1:5000';
 
 export const API_LISTS = `${HOST}/lists`;
 export const API_ITEMS = `${HOST}/items`;
